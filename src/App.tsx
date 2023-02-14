d87b57be5897ba4bdefec1b59f3a607aadd23d1c
Rickson <rickson.simoes@hotmail.com>
2023-02-14 17:31:54 -0300

    :hammer: App: Global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Router } from "./Router";
+import { ThemeProvider } from "styled-components";
+import defaultTheme from '@/styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Header />
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
