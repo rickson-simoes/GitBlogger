8f0935e190f501a4028de578e7ad855f01d33cbc
Rickson <rickson.simoes@hotmail.com>
2023-02-15 14:37:25 -0300

    :art: Header styles

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,15 +4,19 @@ import GitBloggerCover from '@/assets/Cover.svg';
 export const HeaderContainer = styled.header`
   background-image: url(${GitBloggerCover});
   background-repeat: no-repeat;
+  background-position: center;
   background-size: cover;
   box-shadow: inset 0px 1px 9px 7px ${props => props.theme.baseBackground};
+
+  height: 296px;
 
   div {
     display: flex;
     flex-direction: column;
-    gap: 1rem;
     align-items: center;
     justify-content: center;
-    height: 296px;
+
+    gap: 1rem;
+    height: 13.125rem;
   }
 `
