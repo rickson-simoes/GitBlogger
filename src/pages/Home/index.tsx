ed8daa9e77092b3b16ca753413777ae449b32e55
Rickson <rickson.simoes@hotmail.com>
2023-02-15 18:59:50 -0300

    :hammer: Home: Search Posts new component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,12 @@
 import { Profile } from "./components/Profile";
+import { SearchPosts } from "./components/SearchPosts";
 import { Container } from "./styles";
 
 export function Home() {
 	return (
 		<Container>
 			<Profile />
-			HomePage
+			<SearchPosts />
 		</Container>
 	);
 }
