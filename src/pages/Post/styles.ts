21bcd62c27feea11bbf0c980b24f1ed91051da3d
Rickson <rickson.simoes@hotmail.com>
2023-02-17 11:05:30 -0300

    :art: Post page - article

--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -3,5 +3,8 @@ import styled from "styled-components";
 export const PostContainer = styled.main`
     max-width: 54rem;
     margin: 0 auto;
-`
 
+    article {
+        padding: 2.5rem 2rem;
+    }
+`
